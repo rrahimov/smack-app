f741dd537e107d4f636a7ab9ff1e1fe53f645ffa
Ruhullah Rahimov <ruhullah.rahimov@gmail.com>
2021-01-18 22:29:13 +0400

    Alamofire request in code

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -7,7 +7,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ success: Bool) -> ()
+
+//URL constants
+let BASE_URL = "http://localhost:3005/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 //segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//User defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
