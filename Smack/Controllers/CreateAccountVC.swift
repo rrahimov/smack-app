477fda1e61588ea06828a8b95b127961e8febbde
Ruhullah Rahimov <ruhullah.rahimov@gmail.com>
2021-01-30 15:43:26 +0400

    l-14

--- Smack/Controllers/CreateAccountVC.swift
+++ Smack/Controllers/CreateAccountVC.swift
@@ -22,8 +22,13 @@ class CreateAccountVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+        }
     }
     
     
--- Smack/Views/AvatarCell.swift
+++ Smack/Views/AvatarCell.swift
@@ -7,12 +7,27 @@
 
 import UIKit
 
+enum AvatarType {
+    case dark
+    case light
+}
+
 class AvatarCell: UICollectionViewCell {
-    @IBOutlet weak var avatarImg: UIView!
+    @IBOutlet weak var avatarImg: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         setUpView()
+    }
+    
+    func configureCell(index: Int, type: AvatarType) {
+        if type == AvatarType.dark {
+            avatarImg.image = UIImage(named: "dark\(index)")
+            self.layer.backgroundColor = UIColor.lightGray.cgColor
+        } else {
+            avatarImg.image = UIImage(named: "light\(index)")
+            self.layer.backgroundColor = UIColor.darkGray.cgColor
+        }
     }
     
     func setUpView() {
